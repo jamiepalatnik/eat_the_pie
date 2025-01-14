8777d113ea35f4506a155c47fa67839f21f22e12
Kevin O'Connor <kjoconnor@gmail.com>
2025-01-14 15:39:47 -0500

    Cleanup and add word lists

--- eat_the_pie.py
+++ eat_the_pie.py
@@ -15,7 +15,7 @@ def validate_guess(guess, guessed_letters):
     return (True, "")
 
 
-def generate_secret_word():
+def generate_secret_word(difficulty_level):
     word_list = [
         "recurse",
         "apple",
@@ -40,39 +40,69 @@ def check_guess(secret_word, guess):
 
     return indexes
 
+
 def display_pie_chart(starting_pie_slices, pie_slices_remaining):
     # starting_pie_slices 8
     # pie_slices 3
     pie_slices = [
-        {"name": "Pie Slices Remaining", "value": pie_slices_remaining, "color": "#CA8606" },
-        {"name": "Eaten", "value": (starting_pie_slices - pie_slices_remaining), "color": "#fffff"},
+        {
+            "name": "Pie Slices Remaining",
+            "value": pie_slices_remaining,
+            "color": "#CA8606",
+        },
+        {
+            "name": "Eaten",
+            "value": (starting_pie_slices - pie_slices_remaining),
+            "color": "#ffffff",
+        },
     ]
 
     pie = Pie(
         pie_slices,
-        radius=5,
+        radius=10,
         autocolor_pastel_factor=0.7,
         legend={"line": 0, "format": "{label} {name:<8} {percent:>5.2f}% [{value}]"},
     )
+    print("")
     print(pie.render())
+
 
 def main():
     print("Welcome to Eat the Pie!")
-    starting_pie_slices = 8
-    pie_slices = 8
+    print("")
+
+    while True:
+        print("Choose your difficulty")
+        print("1. a-la-mode [easy]")
+        print("2. apple [normal]")
+        print("3. humble [hard]")
+        difficulty_level = input("1 2 or 3?: ")
+
+        if difficulty_level == "":
+            difficulty_level = "2"
+
+        if difficulty_level in ["1", "2", "3"]:
+            break
+
+    match difficulty_level:
+        case "1":
+            starting_pie_slices = pie_slices = 12
+        case "2":
+            starting_pie_slices = pie_slices = 8
+        case "3":
+            starting_pie_slices = pie_slices = 6
+
     display_pie_chart(starting_pie_slices, pie_slices)
 
-    secret_word = generate_secret_word()
-    print(f"Secret word: {secret_word}")
+    secret_word = generate_secret_word(difficulty_level)
+    # print(f"Secret word: {secret_word}")
 
     letters = len(secret_word)
- 
 
     display_word = list("_" * letters)
-    print(' '.join(display_word))
+    print(" ".join(display_word))
 
     guessed_letters = []
-    
 
     while True:
         guess = input("Guess a letter: ")
@@ -81,7 +111,8 @@ def main():
         validate_result = validate_guess(guess, guessed_letters)
         if validate_result[0] is False:
             print(validate_result[1])
-        
+            continue
+
         # Add letter to guessed letters
         guessed_letters.append(guess)
 
@@ -96,8 +127,7 @@ def main():
             for idx in correct_indexes:
                 display_word[idx] = guess
         display_pie_chart(starting_pie_slices, pie_slices)
-        print(' '.join(display_word))
-
+        print(" ".join(display_word))
 
         # Next up:
         # Game end conditions
